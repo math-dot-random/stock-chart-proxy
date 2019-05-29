db1eb995876fbc93ae2eae02101c39ac465f91df
Cokane415 <catherineaokane@gmail.com>
2019-05-28 23:15:06 -0700

    Add new routes

--- server/index.js
+++ server/index.js
@@ -15,10 +15,55 @@ app.listen(port, () => {
     console.log('Server is listening on port 3000')
 });
 
+app.get('/stocks/:ticker', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/index.html'));
+});
+
+//port 3001: Stock Chart
 app.get('/api/stocks/:ticker/prices/:type', (req, res) => {
   request(`http://localhost:3001/api/stocks/${req.params.ticker}/prices/${req.params.type}`, (err, response) => {
     if(err) {
       console.error(err)
     }
   }).pipe(res);
-  })
+});
+
+//port 3002: Earnings Chart
+app.get('/api/stocks/:stock/earnings', (req, res) => {
+  request(`http://localhost:3003/api/stocks/${req.params.stock}/earnings`, (err, response) => {
+    if(err) {
+      console.error(err)
+    }
+  }).pipe(res);
+});
+'/api/stocks/:stock/earnings'
+
+//port 3003: Trade Panel
+app.get('/api/stocks/:stock/price', (req, res) => {
+  request(`http://localhost:3002/api/stocks/${req.params.tock}/price`, (err, response) => {
+    if(err) {
+      console.error(err)
+    }
+  }).pipe(res);
+});
+
+//port 3004: Price Paid Chart 
+app.get('/api/stocks/:stock/', (req, res) => {
+  request(`http://localhost:3004/api/stocks/${req.params.stock}`, (err, response) => {
+    if(err) {
+      console.error(err)
+    }
+  }).pipe(res);
+});
+
+//port 3005: Suggested
+app.get('/api/stocks/:stock/', (req, res) => {
+  request(`http://localhost:3004/api/stocks/${req.params.ticker}`, (err, response) => {
+    if(err) {
+      console.error(err)
+    }
+  }).pipe(res);
+});
+
+
+
