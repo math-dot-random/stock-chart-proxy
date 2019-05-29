cb7f227a30cc027c7069b6b785e9e00546490255
Cokane415 <catherineaokane@gmail.com>
2019-05-28 20:02:34 -0700

    Add a router function for the stock chart component

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+var httpProxy = require('http-proxy');
+const request = require('request');
 
 const app = express();
 
-var port = 3001;
+//create a proxy server
+const proxy = httpProxy.createProxyServer({});
 
+app.use(cors());
 app.use(express.static(path.join(__dirname + '/../public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log('Server is listening on port 3001')
+    console.log('Server is listening on port 3000')
 });
 
-//at this path, load this webpage
-app.get('/stocks/:stock', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public/index.html'));
-  });
+app.get('/api/stocks/:ticker/prices/:type', (req, res) => {
+  request(`http://localhost:3001/api/stocks/${req.params.ticker}/prices/${req.params.type}`, (err, response) => {
+    if(err) {
+      console.error(err)
+    }
+  }).pipe(res);
+  })
+
+  
